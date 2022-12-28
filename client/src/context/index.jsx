6c3297fad3150629b8b1a1bf6884d7fc2f7432bd
Diogo0602x <diogoteixeira0602@gmail.com>
2022-12-28 12:17:33 -0300

    CampaignDetails done

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -54,6 +54,28 @@ export const StateContextProvider = ({ children }) => {
     return filteredCampaigns;
   }
 
+  const donate = async (pId, amount) => {
+    const data = await contract.call('donateToCampaign', pId, {value: ethers.utils.parseEther(amount)});
+
+    return data
+  }
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call('getDonators', pId);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for( let i = 0; i< numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString())
+      })
+    }
+
+    return parsedDonations;
+  }
+
   return (
     <StateContext.Provider
       value={{ 
@@ -62,7 +84,9 @@ export const StateContextProvider = ({ children }) => {
         connect,
         createCampaign: publishCampaign,
         getCampaigns,
-        getUserCampaigns
+        getUserCampaigns,
+        donate,
+        getDonations
       }}
     >
       {children}
